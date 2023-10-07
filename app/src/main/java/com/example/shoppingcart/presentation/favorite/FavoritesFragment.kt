ff2716ab59e5bf71e26d70c1892441c16e9db987
shubham423 <shubh991172@gmail.com>
2023-10-07 19:49:29 +0530

    add categories dialog

--- app/src/main/java/com/example/shoppingcart/presentation/favorite/FavoritesFragment.kt
+++ app/src/main/java/com/example/shoppingcart/presentation/favorite/FavoritesFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.shoppingcart.databinding.FragmentFavoritesBinding
 import com.example.shoppingcart.presentation.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>() {
 
 
--- app/src/main/java/com/example/shoppingcart/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/shoppingcart/presentation/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.example.shoppingcart.presentation.home
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -33,29 +34,41 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
         dialogBinding= DialogCatogoriesBinding.inflate(layoutInflater)
         dialog= Dialog(requireContext())
+        categoryFilterAdapter = CategoryFilterAdapter()
         val categoryAdapter = CategoryAdapter()
         binding.rvProducts.layoutManager = LinearLayoutManager(requireContext())
         binding.rvProducts.adapter = categoryAdapter
         viewModel.getCategories().observe(viewLifecycleOwner) { categories ->
            categoryAdapter.submitList(categories)
-            categoryFilterAdapter.submitList(categories)
+        }
+
+        initClickListeners()
+    }
+
+    private fun initClickListeners() {
+        binding.btnCategories.setOnClickListener {
+            showCustomDialog()
         }
     }
 
     private fun showCustomDialog() {
         val layoutManager = LinearLayoutManager(requireContext())
         dialogBinding.rvCategories.layoutManager = layoutManager
-
-        categoryFilterAdapter = CategoryFilterAdapter( )
         dialogBinding.rvCategories.adapter = categoryFilterAdapter
 
         val alertDialog = AlertDialog.Builder(requireContext())
             .setView(dialogBinding.root)
-            .setPositiveButton("OK") { dialog, _ ->
-                dialog.dismiss()
-            }
             .create()
         alertDialog.show()
+        val layoutParams = alertDialog.window?.attributes
+        layoutParams?.y = 200
+
+        alertDialog.window?.attributes = layoutParams
+        alertDialog.window?.setGravity(Gravity.BOTTOM)
+
+        viewModel.getCategories().observe(viewLifecycleOwner) { categories ->
+            categoryFilterAdapter.submitList(categories)
+        }
     }
 }
 
